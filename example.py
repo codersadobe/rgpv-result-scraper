99073d9ad785624ff3888590821e78041c718615
aakash714 <aakashsingh1407@gmail.com>
2020-01-29 18:02:28 +0530

    initial commit

--- example.py
+++ example.py
@@ -4,10 +4,6 @@ This file demonstrates the usage of the tool.
 
 import rgpvscrape as r
 
-# The following statement fetches the 6th semester results for all 
-# roll numbers between 0208CS161001 and 0208CS161125.
-# Note that the last 4 digits of the roll numbers are part of the
-# roll number's "range"
 r.scrape(college_code='0103',
          branch='CS',
          year=19,
